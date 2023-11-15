a5bc83dd0dfef8f7bbadd70f2f7adfb39adfeac5
Stepan Karpov <stepan.karpov@list.ru>
2023-11-15 13:35:02 +0300

    useless tests removed

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ from grammar import get_copy_of_grammar
 
 def test_chomsky():
     initial_grammar = Grammar.generate_random_grammar()
-    words_step1 = initial_grammar.backtrack_words(7)
+    initial_grammar.output_grammar()
+    words_step1 = initial_grammar.backtrack_words(8)
 
     grammar_to_change = get_copy_of_grammar(initial_grammar)
     grammar_to_change.chomsky_do()
+    grammar_to_change.output_grammar()
     assert grammar_to_change.has_eps() == initial_grammar.has_eps()
-    words_step2 = grammar_to_change.backtrack_words(3)
+    words_step2 = grammar_to_change.backtrack_words(2)
     for word in words_step2:
         if (word not in words_step1):
             assert False
@@ -17,7 +19,7 @@ def test_chomsky():
 def read_test_chomsky():
     initial_grammar = Grammar()
     initial_grammar.read_grammar()
-    words_step1 = initial_grammar.backtrack_words(8)
+    words_step1 = initial_grammar.backtrack_words(5)
 
     grammar_to_change = get_copy_of_grammar(initial_grammar)
     grammar_to_change.chomsky_do()
@@ -29,7 +31,7 @@ def read_test_chomsky():
 
 def default_test_chomsky():
     initial_grammar = Grammar.default_grammar3()
-    words_step1 = initial_grammar.backtrack_words(8)
+    words_step1 = initial_grammar.backtrack_words(7)
 
     grammar_to_change = get_copy_of_grammar(initial_grammar)
     grammar_to_change.chomsky_do()
@@ -46,15 +48,12 @@ def test_cocke_younger_kasami_check():
 
     for word in words_step1:
         if (not grammar.cocke_younger_kasami_check(word)):
-            print(word)
-            grammar.output_grammar()
-            init.output_grammar()
             assert False
 
 def read_test_cocke_younger_kasami_check():
     grammar = Grammar()
     grammar.read_grammar()
-    words_step1 = grammar.backtrack_words(8)
+    words_step1 = grammar.backtrack_words(5)
 
     for word in words_step1:
         if (not grammar.cocke_younger_kasami_check(word)):
@@ -73,11 +72,11 @@ def default_test_cocke_younger_kasami_check():
 
 # read_test_cocke_younger_kasami_check()
 # default_test_cocke_younger_kasami_check()
-tests = 100
-for i in range(0, tests):
-    test_chomsky()
-print(f'{tests} tests passed')
-tests = 100
+# tests = 1000
+# for i in range(0, tests):
+#     test_chomsky()
+# print(f'{tests} tests passed')
+tests = 1000
 for i in range(0, tests):
     test_cocke_younger_kasami_check()
 print(f'{tests} tests passed')
